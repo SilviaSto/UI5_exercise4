b3582fd9cbb8451814d950c36cf9609d21e7def9
SilviaSto <stoianova.silvia@yahoo.com>
2020-12-09 16:29:14 +0000

    Move main view in Albums view

--- webapp/controller/Albums.controller.js
+++ webapp/controller/Albums.controller.js
@@ -1,45 +1,15 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
+		"sap/ui/core/mvc/Controller"
+	],
+	function (Controller) {
+		"use strict";
 
-	return Controller.extend("kpmg.home.exercise4.controller.Albums", {
+		return Controller.extend("kpmg.home.exercise4.controller.Albums", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf kpmg.home.exercise4.view.Albums
-		 */
-		onInit: function () {
-
-		},
-
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf kpmg.home.exercise4.view.Albums
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf kpmg.home.exercise4.view.Albums
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf kpmg.home.exercise4.view.Albums
-		 */
-		//	onExit: function() {
-		//
-		//	}
+			onItemPress: function () {
+					var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				oRouter.navTo("PhotoDetails");
+			}
+		});
 
 	});
-
-});
--- webapp/controller/PhotoDetails.controller.js
+++ webapp/controller/PhotoDetails.controller.js
@@ -1,45 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	 "sap/ui/core/mvc/Controller"
 ], function (Controller) {
 	"use strict";
 
-	return Controller.extend("kpmg.home.exercise4.controller.PhotoDetails", {
-
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf kpmg.home.exercise4.view.PhotoDetails
-		 */
-		onInit: function () {
-
-		},
-
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf kpmg.home.exercise4.view.PhotoDetails
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf kpmg.home.exercise4.view.PhotoDetails
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf kpmg.home.exercise4.view.PhotoDetails
-		 */
-		//	onExit: function() {
-		//
-		//	}
-
-	});
-
+	return Controller.extend("kpmg.home.exercise4.controller.PhotoDetails", {});
 });
