f83c272fa93444dbe779c23afc33a14c5e4ec044
SilviaSto <stoianova.silvia@yahoo.com>
2020-12-10 08:12:12 +0000

    Adjust the AlbumDetails router and path

--- webapp/controller/Albums.controller.js
+++ webapp/controller/Albums.controller.js
@@ -4,9 +4,43 @@ sap.ui.define([
 	function (BaseController) {
 		"use strict";
 		return BaseController.extend("kpmg.home.exercise4.controller.Albums", {
-			onItemPress: function (event) {
-				this.navTo("AlbumDetails");
-			}
+			onInit: function () {
+			var router = this.getRouter();
+			router.getRoute("AlbumDetails").attachMatched(this._onRouteMatched, this);
+		},
+		_onRouteMatched : function (event) {
+			var args, view;
+			args = event.getParameter("arguments");
+			view = this.getView();
+
+			view.bindElement({
+				path : "/AlbumSet(" + args.AlTitle + ")",
+				events : {
+					change: this._onBindingChange.bind(this),
+					dataRequested: function (event) {
+						view.setBusy(true);
+					},
+					dataReceived: function (event) {
+						view.setBusy(false);
+					}
+				}
+			});
+		},
+		
+
+			// onItemPress: function (event) {
+			// 	this.navTo("AlbumDetails");
+			// }
+			
+			
+			onItemPress : function(event){
+			var item, ctx;
+			item = event.getSource();
+			ctx = item.getBindingContext();
+			this.getRouter().navTo("AlbumDetails",{
+				AlTitle : ctx.getProperty("AlTitle")
+			});
+		}
 		});
 
 	});
